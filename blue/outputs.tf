235a627cf66b7462221434c3b0f1e8193ae2662b
krispigeau <kris.pigeau@gmail.com>
2023-04-17 01:46:49 +0000

    removed debug outputs

--- blue/outputs.tf
+++ blue/outputs.tf
@@ -1,17 +1,5 @@
 # Blue module outputs
 
-output "blue_output_vpc_id" {
-  value = module.vpc.output_vpc_id
-}
-
-output "blue_output_sg_id" {
-  value = module.sg.output_sg_id
-}
-
-output "blue_output_sub_ids" {
-  value = module.vpc.output_sub_ids
-}
-
 output "blue_alb_dns_name" {
   value       = module.ha.alb_dns_name
   description = "DNS name of the load balancer"
--- green/outputs.tf
+++ green/outputs.tf
@@ -1,17 +1,5 @@
 # Green module outputs
 
-output "green_output_vpc_id" {
-  value = module.vpc.output_vpc_id
-}
-
-output "green_output_sub_ids" {
-  value = module.vpc.output_sub_ids
-}
-
-output "green_output_sg_id" {
-  value = module.sg.output_sg_id
-}
-
 output "green_alb_dns_name" {
   value       = module.ha.alb_dns_name
   description = "DNS name of the load balancer"
